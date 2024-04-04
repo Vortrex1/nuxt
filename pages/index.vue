0975c504566039f7f1c5a1351757669cd89c2123
Vortrex1 <agladunenko@gmail.com>
2024-04-04 11:36:41 +0300

    feat: update menu(home page)

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
 </script>
 
 <template>
-  <div>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores assumenda, enim facilis iste laudantium nemo, nihil non, porro quasi quis ratione sed sequi tempora temporibus voluptate! Dolorum inventore qui sequi.
+  <div style="color: white; text-align: center; font-weight: 700;">
+Hladunenko Corporation, nuxt project
   </div>
 </template>
 
